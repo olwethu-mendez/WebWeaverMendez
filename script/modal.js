6e98899ea639440c361b9609221db22c34363df7
Olwethu Mzileni <s220428336@mandela.ac.za>
2023-07-22 00:44:17 +0200

    added info to back of cards

--- script/modal.js
+++ script/modal.js
@@ -1,34 +1,35 @@
 function modal(openModalSelector, modalWrapperSelector, modalContentSelector, closeModal = false){
-    const openModalBtn = document.querySelector(openModalSelector);
-    const modalWrapperElement = document.querySelector(modalWrapperSelector);
-    const modalContentElement = document.querySelector(modalContentSelector);
+    const openModalBtn = document.querySelectorAll(openModalSelector);
+    const modalWrapperElement = document.querySelectorAll(modalWrapperSelector);
+    const modalContentElement = document.querySelectorAll(modalContentSelector);
 
-    modalWrapperElement.classList.add("modal-wrapper");
-    modalContentElement.classList.add("modal-content");
-    openModalBtn.classList.add("open-modal")
+    for(let i = 0; i < openModalBtn.length; i++){
+        modalWrapperElement[i].classList.add("modal-wrapper");
+        modalContentElement[i].classList.add("modal-content");
+        openModalBtn[i].classList.add("open-modal")
 
 
-    openModalBtn.addEventListener("click", () =>{
-        modalWrapperElement.style.display = "block";
-    });
-
-    modalWrapperElement.addEventListener("click", (event) => {
-        if(event.target === modalWrapperElement){
-            modalWrapperElement.style.display = "none";
-        }
-    });
-
-    startTimerBtn.addEventListener("click", () => {
-        modalWrapperElement.style.display = "none";
-    })
-
-    if(closeModal) {
-        modalContentElement.innerHTML += '<span class="close-modal modal-x">&times;</span>';
-        const closeModalBtn = modalContentElement.querySelector(".close-modal");
-
-        closeModalBtn.addEventListener("click", () => {
-            modalWrapperElement.style.display = "none";
+        openModalBtn[i].addEventListener("click", () =>{
+            modalWrapperElement[i].style.display = "block";
         });
-    }
 
+        modalWrapperElement[i].addEventListener("click", (event) => {
+            if(event.target === modalWrapperElement[i]){
+                modalWrapperElement[i].style.display = "none";
+            }
+        });
+
+        // startTimerBtn.addEventListener("click", () => {
+        //     modalWrapperElement[i].style.display = "none";
+        // })
+
+        if(closeModal) {
+            modalContentElement[i].innerHTML += '<span class="close-modal modal-x">&times;</span>';
+            const closeModalBtn = modalContentElement[i].querySelector(".close-modal");
+
+            closeModalBtn.addEventListener("click", () => {
+                modalWrapperElement[i].style.display = "none";
+            });
+        }
+    }
 }
